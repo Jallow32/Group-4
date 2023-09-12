7fab16241140996d2498147d2226a79797ac2359
zakariye <zakariye1942@icloud.com>
2023-09-12 14:59:41 +0200

    inget

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Fetchget from './Components/Fetchget';
 
 
 function App() {
 return (
-<div></div>)
+<div><Fetchget/></div>)
   
 }
 
--- src/Components/Fetchget.js
+++ src/Components/Fetchget.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import '../Components/Fetchget.css'
 export default function Fetchget() {
   return (
     <>
