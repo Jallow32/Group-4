62731ab4c54f6c7bc4656595414138cb2bc906ea
Mohamed Yusuf <momme40@hotmail.com>
2023-09-13 13:28:49 +0200

    klar

--- src/Components/Fetchget.js
+++ src/Components/Fetchget.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import '../Components/Fetchget.css'
 
@@ -6,10 +6,32 @@ import '../Components/Fetchget.css'
 // get characters filen 
 
 export default function Fetchget() {
-  const [characters, setCharacters] = useState
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    try {
+      const response = await axios.get('https://thronesapi.com/api/v2/Characters');
+      setCharacters(response.characters)
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacters()
+  }, []);
 
   return (
-    <>
-    </>
+   <div className='character-list'>
+    <h2>Karaktärer</h2>
+    <ul>
+    {characters.map((character) => (
+          <li key={character.id}>{character.fullName}</li>
+        ))}
+    </ul>
+
+   </div>
   )
 }
