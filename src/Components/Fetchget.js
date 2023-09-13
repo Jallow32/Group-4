ac7d37bdd3ccd367369d1d05e313bee4e1990f4e
Mohamed Yusuf <momme40@hotmail.com>
2023-09-13 14:36:38 +0200

    getcharacters är klar

--- src/Components/Fetchget.js
+++ src/Components/Fetchget.js
@@ -1,37 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../Components/Fetchget.css'
-
-
-// get characters filen 
 
 export default function Fetchget() {
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchCharacters = async () => {
     try {
       const response = await axios.get('https://thronesapi.com/api/v2/Characters');
-      setCharacters(response.characters)
-      
+      setCharacters(response.data);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error)
-      
+      console.error(error);
+      setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchCharacters()
+    fetchCharacters();
   }, []);
 
   return (
-   <div className='character-list'>
-    <h2>Karaktärer</h2>
-    <ul>
-    {characters.map((character) => (
-          <li key={character.id}>{character.fullName}</li>
-        ))}
-    </ul>
-
-   </div>
-  )
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+    <h2>Karaktärer</h2> 
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          {characters.map((character) => (
+            <li
+              key={character.id}
+              style={{
+                border: '1px solid #ccc',
+                padding: '10px',
+                margin: '10px',
+                width: '200px',
+                textAlign: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold' }}>
+                {character.fullName} {character.lastName}
+              </p>
+              <p style={{ fontStyle: 'italic' }}>{character.title}</p>
+              <p style={{ color: '#777' }}>{character.family}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
