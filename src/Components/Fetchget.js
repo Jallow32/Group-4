a70c3c42fcb92158998fc6c917f0b692aeb2c0c0
zakariye <zakariye1942@icloud.com>
2023-09-16 11:21:26 +0200

    center image under fullname

--- src/Components/Fetchget.js
+++ src/Components/Fetchget.js
@@ -146,32 +146,39 @@ export default function Fetchget() {
         }}
       >
         {selectedCharacter && (
-          <Card>
+          <Card >
             <CardHeader
-              title="Character Detail"
-              subheader={selectedCharacter.fullName}
-            />
-            <CardContent>
-              <img
-                src={selectedCharacter.imageUrl}
-                alt={selectedCharacter.fullName}
-                style={{
-                  width: '100%',
-                  height: 'auto',
-                  maxWidth: '200px', // Adjust the width as needed
-                }}
-              />
-              <Typography variant="body1">Name: {selectedCharacter.fullName}</Typography>
-              <Typography variant="body1">Title: {selectedCharacter.title}</Typography>
-              <Typography variant="body1">Family: {selectedCharacter.family}</Typography>
+  title={<div style={{ textAlign: 'left' }}>Character Detail</div>}
+  style={{ textAlign: 'center' }}
+/>
+<CardContent>
+  <div style={{ textAlign: 'center' }}>
+    <div>
+      <Typography variant="h5">{selectedCharacter.fullName}</Typography>
+    </div>
+    <div>
+      <img
+        src={selectedCharacter.imageUrl}
+        alt={selectedCharacter.fullName}
+        style={{
+          width: '100%',
+          height: 'auto',
+          maxWidth: '200px',
+          margin: '0 auto', 
+        }}
+      />
+    </div>
+  </div>
+  <Typography variant="body1">First Name: {selectedCharacter.firstName}</Typography>
+  <Typography variant="body1">Last Name: {selectedCharacter.lastName}</Typography>
+  <Typography variant="body1">Title: {selectedCharacter.title}</Typography>
+  <Typography variant="body1">Family: {selectedCharacter.family}</Typography>
 
-              {/* Additional Details */}
-              <Typography variant="body1">First Name: {selectedCharacter.firstName}</Typography>
-              <Typography variant="body1">Last Name: {selectedCharacter.lastName}</Typography>
-              <Typography variant="body1">Full Name: {selectedCharacter.fullName}</Typography>
-              <Typography variant="body1">Image: {selectedCharacter.image}</Typography>
-              <Typography variant="body1">Image URL: {selectedCharacter.imageUrl}</Typography>
-            </CardContent>
+  
+  <Typography variant="body1">Image: {selectedCharacter.image}</Typography>
+  <Typography variant="body1">Image URL: {selectedCharacter.imageUrl}</Typography>
+</CardContent>
+
           </Card>
         )}
       </div>
